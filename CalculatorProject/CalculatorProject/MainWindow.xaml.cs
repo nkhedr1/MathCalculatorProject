a4d95d8ee7fb404bef44cfe2425803e5f13a990b
Nour Mohammed <noureldinkhedr85@gmail.com>
2019-05-21 15:10:49 -0500

    factorial

--- CalculatorProject/CalculatorProject/MainWindow.xaml.cs
+++ CalculatorProject/CalculatorProject/MainWindow.xaml.cs
@@ -22,6 +22,9 @@ namespace CalculatorProject
 
         public string currentFormula { get; set; }
         public string fullFormula { get; set; }
+        public string powerBaseValue { get; set; }
+        public string powerValue { get; set; }
+        public bool power { get; set; }
 
 
         public MainWindow()
@@ -30,6 +33,7 @@ namespace CalculatorProject
             this.Title = "Calculator Project";
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
             OutputWindow.Text = "0";
+            power = false;
 
         }
 
@@ -41,7 +45,7 @@ namespace CalculatorProject
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             currentFormula = 1.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula,currentFormula);
             OutputWindow.Text = fullFormula;
             Calculation(fullFormula);
 
@@ -50,14 +54,14 @@ namespace CalculatorProject
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             currentFormula = 2.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
             currentFormula = 3.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
      
@@ -65,82 +69,130 @@ namespace CalculatorProject
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
             currentFormula = 4.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
             currentFormula = 5.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
             currentFormula = 6.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_7(object sender, RoutedEventArgs e)
         {
             currentFormula = 7.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_8(object sender, RoutedEventArgs e)
         {
             currentFormula = 8.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_9(object sender, RoutedEventArgs e)
         {
             currentFormula = 9.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_0(object sender, RoutedEventArgs e)
         {
             currentFormula = 0.ToString();
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
+            OutputWindow.Text = fullFormula;
+        }
+
+        private void Button_Click_Decimal(object sender, RoutedEventArgs e)
+        {
+            currentFormula = ".";
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_Add(object sender, RoutedEventArgs e)
         {
             currentFormula = "+";
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_Subtract(object sender, RoutedEventArgs e)
         {
             currentFormula = "-";
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_Divide(object sender, RoutedEventArgs e)
         {
             currentFormula = "/";
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
+            OutputWindow.Text = fullFormula;
+        }
+
+        private void Button_Click_Mod(object sender, RoutedEventArgs e)
+        {
+            currentFormula = "%";
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_Multiply(object sender, RoutedEventArgs e)
         {
             currentFormula = "*";
-            fullFormula = String.Concat(currentFormula, fullFormula);
+            fullFormula = String.Concat(fullFormula, currentFormula);
             OutputWindow.Text = fullFormula;
         }
 
         private void Button_Click_Equal(object sender, RoutedEventArgs e)
         {
+            Calculation(fullFormula);
+
+            if (power == true)
+            {
+                CalculationPower(powerBaseValue, fullFormula);
+            }
+        }
+
+        private void Button_Click_SquareRoot(object sender, RoutedEventArgs e)
+        {
+            int input = Int32.Parse(fullFormula);
+            var squareRoot = Math.Sqrt(input);
+            OutputWindow.Text = squareRoot.ToString();
+        }
+
+        private void Button_Click_Power(object sender, RoutedEventArgs e)
+        {
+            powerBaseValue = fullFormula;
+            fullFormula = "";
+
+            power = true;
+        }
+
+        private void Button_Click_Factorial(object sender, RoutedEventArgs e)
+        {
+            decimal baseNumber = Int32.Parse(fullFormula);
+            decimal z = 1;
+            for (decimal i = 1; i < baseNumber; i++)
+            {
+                z = (baseNumber - i);
+                currentFormula = String.Concat("*", z.ToString());
+                fullFormula = String.Concat(fullFormula, currentFormula);
+            }
+
             Calculation(fullFormula);
         }
 
@@ -154,9 +206,17 @@ namespace CalculatorProject
         private void Calculation(string fullFormula)
         {
             NCalc.Expression e = new NCalc.Expression(fullFormula);
-            e.Evaluate().ToString();
-            OutputWindow.Text = e.Evaluate().ToString();
+            var x = e.Evaluate();
+            OutputWindow.Text = x.ToString();
         }
+
+        private void CalculationPower(string powerBaseValue, string fullFormula)
+        {
+            var powerResult = Math.Pow(Int32.Parse(powerBaseValue), Int32.Parse(fullFormula));
+            power = false;
+            OutputWindow.Text = powerResult.ToString();
+        }
+
 
     }
 }
